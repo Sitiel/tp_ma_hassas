4d56004d0183ecf05bd1520b641fdc04ae7e87b4
Valérian <valerian.acier@hotmail.fr>
2019-11-12 17:00:00 +0100

    Add new features

--- main.py
+++ main.py
@@ -6,7 +6,6 @@ import random
 import argparse
 from collections import Counter
 
-
 # ----------- ARGS
 
 parser = argparse.ArgumentParser(description='N-Puzzle game')
@@ -21,7 +20,8 @@ parser.add_argument("--kp", default=0.1, help="Constant Kp prise")
 parser.add_argument("--kd", default=0.3, help="Constant Kd depot")
 parser.add_argument("--i", default=1, help="I possible move between actions")
 parser.add_argument("--error", default=0, help="Error rate in recognition")
-
+parser.add_argument("--levy", default=0, help="Levy flight probability")
+parser.add_argument("--view", default=0, help="use vision instead of memory")
 
 # ---------- INIT
 
@@ -37,15 +37,18 @@ kp = float(args.kp)
 kd = float(args.kd)
 i_move = int(args.i)
 error_rate = float(args.error)
+levy = float(args.levy)
+view = int(args.view)
 
+objects_letter = ['A', 'B']
 
 message_box = {}
 
-BLACK = (0,0,0)
-RED = (255,0,0)
-GREEN = (0,255,0)
-BLUE = (0,0,255)
-WHITE = (255,255,255)
+BLACK = (0, 0, 0)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+WHITE = (255, 255, 255)
 YELLOW = (255, 255, 0)
 MAGENTA = (255, 0, 255)
 
@@ -60,14 +63,15 @@ sliderMaxI = 30
 sliderMinI = 1
 sliderMaxError = 1
 sliderMinError = 0
+sliderMaxLevy = 1
+sliderMinLevy = 0
 bestScore = 0
 
 pygame.init()
 screen = pygame.display.set_mode((windowWidth + 300, windowHeight), 0, 32)
 pygame.display.set_caption('Interactions Multi-Agents')
 
-
-directions = [[1,0], [0,1], [-1,0], [0,-1]]
+directions = [[1, 0], [0, 1], [-1, 0], [0, -1]]
 LEFT = 0
 UP = 1
 RIGHT = 2
@@ -81,7 +85,7 @@ class Board:
         self.occupiedCase = [[0 for j in range(self.n)] for i in range(self.n)]
         self.objectOccupiedCase = [["" for j in range(self.n)] for i in range(self.n)]
 
-    def add_object(self, x, y , type):
+    def add_object(self, x, y, type):
         self.objectOccupiedCase[y][x] = type
 
     def can_add_object_at(self, x, y):
@@ -125,16 +129,18 @@ class Agent(threading.Thread):
         self.y = y
         self.id = id
         self.memory = []
-        self.stuck = 0
         self.carry = ""
 
     def random_move(self):
-        ite = random.randint(1, i_move)
+        if random.uniform(0, 1) < levy:
+            ite = random.randint(10, 25)
+        else:
+            ite = random.randint(1, i_move)
         tx, ty = directions[random.randint(0, len(directions) - 1)]
-        return self.try_a_move(self.x + tx*ite, self.y + ty*ite)
+        return self.try_a_move(self.x + tx * ite, self.y + ty * ite)
 
     def try_a_move(self, tx, ty):
-        if tx < 0 or tx > board.n-1 or ty < 0 or ty > board.n-1:
+        if tx < 0 or tx > board.n - 1 or ty < 0 or ty > board.n - 1:
             return "error"
         result = self.board.move(self, tx, ty)
         return result
@@ -145,31 +151,13 @@ class Agent(threading.Thread):
         self.y = ty
         if type == "":
             type = "0"
+        elif random.uniform(0, 1) < error_rate:
+            # choose a random element to see instead of the real one, can be the real one
+            type = random.choice(objects_letter)
         self.memory.append(type)
 
         if len(self.memory) > t:
             self.memory = self.memory[-t:]
-
-    def has_received_a_move(self):
-        if self.id in message_box and len(message_box[self.id]) != 0:
-            for m in message_box[self.id]:
-                if m.x == self.x and m.y == self.y:
-                    return True
-        return False
-
-    def find_another_position(self):
-        possibilities = []
-        for tx, ty in directions:
-            if self.y + ty > self.board.n-1 or self.y+ty < 0 or self.x + tx < 0 or self.x + tx > self.board.n - 1:
-                continue
-            if self.board.occupiedCase[self.y + ty][self.x + tx] > 0:
-                continue
-            possibilities.append([tx, ty])
-
-        if len(possibilities) == 0:
-            possibilities = directions
-
-        return possibilities[random.randint(0, len(possibilities)-1)]
 
     def run(self):
         while True:
@@ -177,6 +165,14 @@ class Agent(threading.Thread):
                 time.sleep(speed)
 
             type = self.board.get_object_at(self.x, self.y)
+
+            if view == 1:
+                current_view = []
+                for tx in range(self.x - 3, self.x + 3):
+                    for ty in range(self.y - 3, self.y + 3):
+                        if n > tx >= 0 and n > ty >= 0:
+                            current_view.append(self.board.get_object_at(tx, ty))
+                self.memory = current_view
             c = Counter(self.memory)
             f = 0
 
@@ -195,7 +191,7 @@ class Agent(threading.Thread):
                     continue
 
             if f > 0 and len(type) != 0 and self.carry == "":
-                p_prise = (kp/(kp+f))*(kp/(kp+f))
+                p_prise = (kp / (kp + f)) * (kp / (kp + f))
                 if r < p_prise:
                     # print("Carry")
                     self.carry = board.carry_object(self.x, self.y)
@@ -213,8 +209,8 @@ board = Board(n)
 
 for i in range(agents_number):
     while True:
-        x = random.randint(0, board.n-1)
-        y = random.randint(0, board.n-1)
+        x = random.randint(0, board.n - 1)
+        y = random.randint(0, board.n - 1)
         if board.occupiedCase[y][x] == 0:
             break
     a = Agent(board, id, x, y)
@@ -224,7 +220,6 @@ for i in range(agents_number):
 
 basicFont = pygame.font.SysFont(None, 32)
 
-objects_letter = ['A', 'B']
 objects_text = {}
 for letter in objects_letter:
     objects_text[letter] = basicFont.render(letter, True, BLACK)
@@ -233,13 +228,13 @@ i = 0
 while i < objects_number:
     x = random.randint(0, board.n - 1)
     y = random.randint(0, board.n - 1)
-    type = objects_letter[random.randint(0, len(objects_letter)-1)]
+    type = objects_letter[random.randint(0, len(objects_letter) - 1)]
     if board.can_add_object_at(x, y):
         board.add_object(x, y, type)
         i += 1
 
-cellWidth = windowWidth/board.n
-cellHeight = windowHeight/board.n
+cellWidth = windowWidth / board.n
+cellHeight = windowHeight / board.n
 
 for a in agents:
     a.start()
@@ -265,7 +260,7 @@ while True:
                 color = BLUE
 
             pygame.draw.rect(screen, BLACK, (i * cellWidth, j * cellHeight, cellWidth, cellHeight))
-            pygame.draw.rect(screen, color, (i * cellWidth+3, j * cellHeight+3, cellWidth-6, cellHeight-6))
+            pygame.draw.rect(screen, color, (i * cellWidth + 3, j * cellHeight + 3, cellWidth - 6, cellHeight - 6))
 
             if type != "":
                 textRect = objects_text[type].get_rect()
@@ -307,9 +302,18 @@ while True:
     screen.blit(textSlider, area)
 
     slider_error = pygame.draw.rect(screen, BLACK, (windowWidth + 30, 330, 240, 20))
-    pygame.draw.rect(screen, BLACK, (windowWidth + 30 + 240 * ((error_rate + sliderMinError) / sliderMaxError) - 15, 320, 30, 40))
+    pygame.draw.rect(screen, BLACK,
+                     (windowWidth + 30 + 240 * ((error_rate + sliderMinError) / sliderMaxError) - 15, 320, 30, 40))
 
+    textSlider = basicFont.render("levy probability : " + str(levy), True, BLACK)
+    area = textSlider.get_rect()
+    area.centerx = windowWidth + 150
+    area.centery = 390
+    screen.blit(textSlider, area)
 
+    slider_levy = pygame.draw.rect(screen, BLACK, (windowWidth + 30, 420, 240, 20))
+    pygame.draw.rect(screen, BLACK,
+                     (windowWidth + 30 + 240 * ((levy + sliderMinLevy) / sliderMaxLevy) - 15, 410, 30, 40))
 
     pygame.display.update()
     pygame.event.get()
@@ -317,15 +321,16 @@ while True:
         pos = pygame.mouse.get_pos()
         if pos[0] > windowWidth:
             if pos[1] < 100:
-                speed = ((max(windowWidth+30, pos[0]) - (windowWidth + 30)) / 240 + sliderMin)*sliderMax
+                speed = ((max(windowWidth + 30, pos[0]) - (windowWidth + 30)) / 240 + sliderMin) * sliderMax
             elif pos[1] < 200:
                 t = int(((max(windowWidth + 30, pos[0]) - (windowWidth + 30) + sliderMinT) / 240) * sliderMaxT)
             elif pos[1] < 300:
-                i_move = max(1, int(((max(windowWidth + 30, pos[0]) - (windowWidth + 30) + sliderMinI) / 240) * sliderMaxI))
+                i_move = max(1, int(
+                    ((max(windowWidth + 30, pos[0]) - (windowWidth + 30) + sliderMinI) / 240) * sliderMaxI))
             elif pos[1] < 400:
-                error_rate = (((max(windowWidth + 30, pos[0]) - (windowWidth + 30) + sliderMinError) / 240) * sliderMaxError)
+                error_rate = (((max(windowWidth + 30, pos[0]) - (
+                            windowWidth + 30) + sliderMinError) / 240) * sliderMaxError)
+            elif pos[1] < 500:
+                levy = (((max(windowWidth + 30, pos[0]) - (
+                        windowWidth + 30) + sliderMinLevy) / 240) * sliderMaxLevy)
     pygame.time.delay(100)
-
-
-
-
